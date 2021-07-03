2e1a847ab7cd0b714c258d76f8192ce826c8f695
Russeevich <kokanazar@yandex.ru>
2021-07-03 12:09:40 +0300

    update v0.1

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const path = require('path')
 const fs = require('fs-extra')
 const glob = require('glob')
 
-const mainDir = path.resolve(process.argv[2])
-const pathMain = path.resolve(process.argv[3])
+const mainDir = path.resolve(__dirname, process.argv[2])
+const pathMain = path.resolve(__dirname, process.argv[3])
 const deleteFolder = process.argv[4]
+
+const types = [
+    { type: ".pdf", dir: 'documents' },
+    { type: ".png", dir: 'images' },
+    { type: ".mp3", dir: "music" }
+]
 
 fs.removeSync(pathMain)
 
 glob(mainDir + '/**/*.*', (err, res) => {
-    const files = res.map(item => ({ name: item.split('/')[item.split('/').length - 1], path: item }))
+    const files = res.map(item => ({ name: item.split('/')[item.split('/').length - 1], path: item, ext: item.split('/')[item.split('/').length - 1].split('.')[1] }))
     if (!fs.existsSync(pathMain))
         fs.mkdirSync(pathMain)
     for (const item of files) {
-        const pathx = path.resolve(`${pathMain}/${item.name[0]}`).toString()
+        const dirPath = path.resolve(`${pathMain}/${types.find(type => type.type.includes(item.ext)) ?types.find(type => type.type.includes(item.ext)).dir : 'other' }`)
+        if (!fs.existsSync(dirPath))
+            fs.mkdirSync(dirPath)
+        const pathx = path.resolve(`${dirPath}/${item.name[0]}`).toString()
         if (!fs.existsSync(pathx))
             fs.mkdirSync(pathx)
         fs.moveSync(item.path, path.resolve(`${pathx}/${item.name}`).toString())
