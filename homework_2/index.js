84b1823b5ac76501ab1bdf3455cde6a2b6b2d4bd
Russeevich <kokanazar@yandex.ru>
2021-07-07 13:48:13 +0300

    update http server

--- homework_2/index.js
+++ homework_2/index.js
@@ -4,35 +4,19 @@ const port = process.env.PORT
 const delay = process.env.DELAY || 1000
 const timestop = process.env.TIMESTOP || 10000
 
-let users = []
-
 app.get('/data', (req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8")
     res.setHeader("Transfer-Encoding", "chunked")
-    users.push({ res, time: Date.now() })
-})
-
-const getDate = () => {
-    const date = new Date(),
-        now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-            date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds())
-
-    return new Date(now_utc)
-}
-
-setInterval(() => {
-    if (users.length > 0) {
-        const date = getDate()
+    setInterval(() => {
+        const date = new Date().toISOString()
         console.log(`Time: ${date}`)
-        users.forEach((user, ndx) => {
-            if (Date.now() - user.time >= timestop) {
-                user.res.write(`Time: ${date}, END`)
-                user.res.end()
-                users = users.filter((_, index) => index !== ndx)
-            }
-        })
-    }
-}, delay)
+    }, delay)
+    setTimeout(() => {
+        const date = new Date().toISOString()
+        res.write(`Time: ${date}, END`)
+        res.end()
+    }, timestop)
+})
 
 
 app.listen(port, () => console.log(`Server start in port ${port}`))
