d382be719bd93aff6623a4510a6d289b987807a5
Russeevich <kokanazar@yandex.ru>
2021-07-07 15:06:27 +0300

    promise added lesson_1

--- homework_1/index.js
+++ homework_1/index.js
@@ -6,46 +6,88 @@ const mainDir = process.argv[2] ? path.resolve(__dirname, process.argv[2]) : pat
 const pathMain = process.argv[3] ? path.resolve(__dirname, process.argv[3]) : path.resolve(__dirname, './test2')
 const deleteFolder = process.argv[4] ? process.argv[4] : false
 
-const takeError = (err) => {
-    if (err) {
-        throw new Error(err)
-    }
-}
-
-fs.remove(pathMain, takeError)
-
 const cpFile = (item, pathx) => {
-    if (deleteFolder === true) {
-        fs.move(item.path, path.resolve(`${pathx}/${item.name}`), takeError)
-    } else {
-        fs.copyFile(item.path, path.resolve(`${pathx}/${item.name}`), takeError)
-    }
+    return new Promise((resolve, reject) => {
+        if (deleteFolder === true) {
+            fs.move(item.path, path.resolve(`${pathx}/${item.name}`), (err) => {
+                if (err) {
+                    reject(new Error(err))
+                }
+
+                resolve('File moved')
+            })
+        } else {
+            fs.copyFile(item.path, path.resolve(`${pathx}/${item.name}`), (err) => {
+                if (err) {
+                    reject(new Error(err))
+                }
+
+                resolve('File copied')
+            })
+        }
+    })
 }
 
-
-glob(mainDir + '/**/*.*', (err, res) => {
-
-    takeError(err)
-
-    const files = res.map(item => ({ name: item.split('/')[item.split('/').length - 1], path: item }))
-
-    fs.mkdir(pathMain, (err) => {
-        takeError(err)
-
-        for (const item of files) {
-            const pathx = path.resolve(`${pathMain}/${item.name[0].toUpperCase()}`)
-
-            if (!fs.pathExists(pathx)) {
-                fs.mkdir(pathx, (err) => {
-                    takeError(err)
-
-                    cpFile(item, pathx)
-                })
-            } else {
-                cpFile(item, pathx)
+const getFile = (dir) => {
+    return new Promise((resolve, reject) => {
+        glob(dir, (err, res) => {
+            if (err) {
+                reject(new Error(err))
             }
+            const files = res.map(item => ({ name: item.split('/')[item.split('/').length - 1], path: item }))
+
+            if (files.length < 1)
+                reject(new Error('No files found'))
+
+            resolve(files)
+        })
+    })
+}
+
+const makeDir = (dirPath) => {
+    return new Promise((resolve, reject) => {
+        if (!fs.pathExistsSync(dirPath)) {
+            fs.mkdir(dirPath, (err) => {
+                if (err) {
+                    reject(new Error(err))
+                }
+                resolve('Dir created')
+            })
+        } else {
+            resolve('OK')
+        }
+    })
+}
+
+const removeFolder = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.remove(path, (err) => {
+            if (err) {
+                reject(new Error(err))
+            }
+
+            resolve('Dir removed')
+        })
+    })
+}
+
+const startApp = async() => {
+    try {
+        await removeFolder(pathMain)
+        const files = await getFile(mainDir + '/**/*.*')
+
+        await makeDir(pathMain)
+
+        for await (const item of files) {
+            const pathx = path.resolve(`${pathMain}/${item.name[0].toUpperCase()}`)
+            await makeDir(pathx)
+            await cpFile(item, pathx)
         }
         if (deleteFolder === 'true')
-            fs.remove(mainDir, takeError)
-    })
-})
+            await removeFolder(mainDir)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+startApp()
